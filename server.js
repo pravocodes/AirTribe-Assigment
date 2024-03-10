7ecdc009ef3e2c5ee882905fd1ec38292ad39118
Praveen Kumar <singhpraveen5673@gmail.com>
2024-03-10 16:15:15 +0530

    Server Created

---
+++ server.js
@@ -0,0 +1,25 @@
+import Express from "express";
+import color from "colors";
+import dotenv from "dotenv";
+import conn from "./dbconfig/db.js";
+import cors from "cors";
+import authRoute from "./routes/authRoute.js";
+import instructorRoute from "./routes/instructorRoute.js";
+const app = Express();
+app.get("/", (req, res) => {
+  res.send("<h1>Welcome</h1>");
+});
+
+dotenv.config();
+
+app.use(Express.json());
+
+app.use(cors());
+app.use("/api/auth", authRoute);
+app.use("/api/instructor", instructorRoute);
+
+conn();
+const port = 5000;
+app.listen(5000, () => {
+  console.log(`Server is running on ${port}`);
+});
